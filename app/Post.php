7df93995bbf58971ae0b1143fb705691c3ccf91a
Gbolahan Olawuyi <gbolahanolawuyi4u@gmail.com>
2018-07-12 19:37:39 +0100

    added fillable fields to post model

--- app/Post.php
+++ app/Post.php
@@ -6,6 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
+
+    protected $fillable = [
+        'user_id', 'title', 'post_slug', 'post_image'
+    ]
+
     public function user()
     {
         return $this->belongsTo('App\User');
