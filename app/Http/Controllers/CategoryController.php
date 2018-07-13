64d347f9f8f18b62b865526037d86b6343728047
Gbolahan Olawuyi <gbolahanolawuyi4u@gmail.com>
2018-07-13 20:06:54 +0100

    created functions to handle route requests

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -3,8 +3,62 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Category;
 
 class CategoryController extends Controller
 {
-    //
+    public function index()
+    {
+       $category = Category::all();
+       return response()->json($category);
+    }
+
+    public function create()
+    {
+
+    }
+
+    public function store(Request $request)
+    {
+
+        $category = Category::create([
+            'name' => $request->name,
+        ]);
+
+        $category->save();
+
+        session()->flash('success', 'Category created');
+    }
+
+    public function edit($id)
+    {
+        $category = Category::findorfail($id);
+
+        return response()->json($category);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $category = Category::findorfail($id);
+
+
+        $category->name = $request->name;
+
+        $category->save();
+
+        session()->flash('success', 'Category Updated');
+    }
+
+    public function destroy($id)
+    {
+        $category = Category::findorfail($id);
+
+        foreach ($category->posts as $post) {
+            $post->forceDelete();
+        }
+
+        $category->delete();
+
+        session()->flash('success', 'Category Deleted');
+    }
 }
