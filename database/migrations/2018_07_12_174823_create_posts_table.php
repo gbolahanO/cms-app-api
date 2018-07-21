da34a8259d1f20858d9c456f2ae274db60701e55
Gbolahan Olawuyi <gbolahanolawuyi4u@gmail.com>
2018-07-21 14:29:01 +0100

    renamed post_body to content and changed the  data type from string to text

--- database/migrations/2018_07_12_174823_create_posts_table.php
+++ database/migrations/2018_07_12_174823_create_posts_table.php
@@ -18,7 +18,7 @@ class CreatePostsTable extends Migration
             $table->integer('user_id');
             $table->string('category_id');
             $table->string('title');
-            $table->string('post_body');
+            $table->text('content');
             $table->char('post_slug');
             $table->char('post_image')->nullable();
             $table->timestamps();
